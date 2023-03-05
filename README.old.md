0e8c515b205e90e31ec0b18b76fb9cfe00c88cfb
Daniel Maher <danielmaher01@gmail.com>
2023-03-04 22:25:17 -0500

    created initial node.js app and started creating navigation bar for app

---
+++ README.old.md
@@ -0,0 +1,7 @@
+This is going to be an all encompassing crypto dashboard
+
+Features:
+	- Track favorite cryptocurrencies
+	- Track news based on ecosystem
+	- Connect cardano wallets
+	- Track portfolio (Tokens, NFTs)
---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+// import './App.css'
+
+const App = () => {
+  return (
+       <div className="app">
+        
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+            {/* <Layout>
+                <div className="routes">
+                    <Routes>
+                        <Route path="/" element={<Homepage/>}>
+                            
+                        </Route>
+                        <Route path="exchanges" element={<Exchanges/>}>
+                            
+                        </Route>
+                        <Route exact path="cryptocurrencies" element={<Cryptocurrencies/>}>
+                            
+                        </Route>
+                        <Route path="crypto/:coinId" element={<CryptoDetails/>}>
+                        </Route>
+                            
+                        
+                        <Route path="News" element={<News/>}>
+                            
+                        </Route>
+                        
+                    </Routes>
+                </div>
+            </Layout> */}
+        </div>
+        <div className="footer">
+
+        </div>
+    </div>
+  );
+}
+
+export default App
